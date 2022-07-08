a998594cd771972a8a7b9106e38f8425b0574f38
Mathias Buus <mathiasbuus@gmail.com>
2022-07-08 15:40:02 +0200

    support seeder swarms

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Corestore = require('corestore')
 const Hyperswarm = require('hyperswarm')
 const Hyperbundle = require('hyperbundle')
 const HypercoreId = require('hypercore-id-encoding')
+const Seeders = require('@hyperswarm/seeders')
 const minimist = require('minimist')
 const goodbye = require('graceful-goodbye')
 
@@ -11,56 +12,84 @@ const argv = minimist(process.argv.slice(2), {
   alias: {
     key: 'k',
     bundle: 'b',
+    seeder: 's'
   }
 })
 
-const store = new Corestore('./corestore')
-const swarm = new Hyperswarm()
-
-const keys = [].concat(argv.key || [])
-const bundles = [].concat(argv.bundle || [])
-
-swarm.on('connection', function (socket) {
-  const p = socket.remotePublicKey.toString('hex')
-
-  console.log('Connection opened', p)
-  store.replicate(socket)
-  socket.on('error', function (err) {
-    console.log(err)
-  })
-  socket.on('close', function () {
-    console.log('Connection closed', p)
-  })
+start().catch(err => {
+  console.error(err)
+  process.exit(1)
 })
 
-for (const key of keys) {
-  downloadCore(key)
-}
-for (const key of bundles) {
-  downloadBundle(key)
-}
-
-async function downloadBundle (key) {
-  const bundleId = HypercoreId.encode(HypercoreId.decode(key))
-  const bundle = new Hyperbundle(store, HypercoreId.decode(key))
-  bundle.on('blobs', blobs => downloadCore(blobs.core, bundleId, false))
-  console.log('downloading bundle', bundleId)
-  downloadCore(bundle.core)
-}
-
-async function downloadCore (core, bundleId, announce) {
-  core = typeof core === 'string' ? store.get(HypercoreId.decode(core)) : core 
-  await core.ready()
-  const id = HypercoreId.encode(core.key)
-  console.log('downloading core', id)
-
-  if (announce !== false) swarm.join(core.discoveryKey)
-  core.download()
-
-  core.on('download', function (index) {
-    const bundleDataTag = bundleId ? 'with bundle ' + bundleId : ''
-    console.log('downloaded block', index, 'from', id, bundleDataTag)
+async function start () {
+  const secretKey = argv['secret-key']
+  const store = new Corestore('./corestore')
+  const swarm = new Hyperswarm({
+    seed: secretKey ? HypercoreId.decode(secretKey) : undefined,
+    keyPair: secretKey ? undefined : await store.createKeyPair('simple-seeder-swarm')
   })
-}
 
-goodbye(() => swarm.destroy())
+  console.log('Starting with with public key', HypercoreId.encode(swarm.keyPair.publicKey))
+
+  const keys = [].concat(argv.key || [])
+  const bundles = [].concat(argv.bundle || [])
+  const seeders = [].concat(argv.seeder || [])
+
+  swarm.on('connection', function (socket) {
+    const p = socket.remotePublicKey.toString('hex')
+
+    console.log('Connection opened', p)
+    store.replicate(socket)
+    socket.on('error', function (err) {
+      console.log(err)
+    })
+    socket.on('close', function () {
+      console.log('Connection closed', p)
+    })
+  })
+
+  swarm.listen()
+
+  for (const key of keys) {
+    downloadCore(key)
+  }
+  for (const key of bundles) {
+    downloadBundle(key)
+  }
+
+  for (const s of seeders) {
+    const sw = new Seeders(HypercoreId.decode(s), {
+      dht: swarm.dht
+    })
+
+    sw.join()
+    sw.on('update', function (record) {
+      console.log('seeder change for ' + s + ':', record)
+    })
+  }
+
+  async function downloadBundle (key) {
+    const bundleId = HypercoreId.encode(HypercoreId.decode(key))
+    const bundle = new Hyperbundle(store, HypercoreId.decode(key))
+    bundle.on('blobs', blobs => downloadCore(blobs.core, bundleId, false))
+    console.log('downloading bundle', bundleId)
+    downloadCore(bundle.core)
+  }
+
+  async function downloadCore (core, bundleId, announce) {
+    core = typeof core === 'string' ? store.get(HypercoreId.decode(core)) : core
+    await core.ready()
+    const id = HypercoreId.encode(core.key)
+    console.log('downloading core', id)
+
+    if (announce !== false) swarm.join(core.discoveryKey)
+    core.download()
+
+    core.on('download', function (index) {
+      const bundleDataTag = bundleId ? 'with bundle ' + bundleId : ''
+      console.log('downloaded block', index, 'from', id, bundleDataTag)
+    })
+  }
+
+  goodbye(() => swarm.destroy())
+}
