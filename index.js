9ad36a88689d3bb8dac3fddfa8b374de7d49db42
Lucas <lucasbarrena98@gmail.com>
2023-04-21 12:00:31 +0000

    Self updating UI

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const HypercoreId = require('hypercore-id-encoding')
 const Seeders = require('@hyperswarm/seeders')
 const minimist = require('minimist')
 const goodbye = require('graceful-goodbye')
-const fsp = require('fs/promises')
+const fs = require('fs')
 const configs = require('tiny-configs')
+const crayon = require('tiny-crayon')
+const speedometer = require('speedometer')
 
 const argv = minimist(process.argv.slice(2), {
   alias: {
@@ -22,9 +24,18 @@ const argv = minimist(process.argv.slice(2), {
   }
 })
 
+const tracking = {
+  intervalId: null,
+  swarm: null,
+  cores: [],
+  bees: [],
+  drives: [],
+  seeders: []
+}
+
 start().catch(err => {
   console.error(err)
-  process.exit(1)
+  process.exit()
 })
 
 async function start () {
@@ -35,17 +46,18 @@ async function start () {
     keyPair: secretKey ? undefined : await store.createKeyPair('simple-seeder-swarm')
   })
 
-  console.log('Starting with with public key', HypercoreId.encode(swarm.keyPair.publicKey))
+  tracking.swarm = swarm
+  update()
 
-  const keys = [].concat(argv.key || [])
+  const cores = [].concat(argv.key || [])
   const bees = [].concat(argv.bee || [])
   const drives = [].concat(argv.drive || [])
   const seeders = [].concat(argv.seeder || [])
 
   if (argv.file) {
-    const seeds = await fsp.readFile(argv.file)
+    const seeds = await fs.promises.readFile(argv.file)
     for (const [type, key] of configs.parse(seeds, { split: ' ', length: 2 })) {
-      if (type === 'key') keys.push(key)
+      if (type === 'key') cores.push(key)
       else if (type === 'bee') bees.push(key)
       else if (type === 'bundle' || type === 'drive') drives.push(key)
       else if (type === 'seeder') seeders.push(key)
@@ -53,108 +65,208 @@ async function start () {
     }
   }
 
-  let connections = 0
-
   swarm.on('connection', onsocket)
-
   swarm.listen()
 
-  for (const key of keys) {
-    downloadCore(key)
-  }
+  for (const key of cores) downloadCore(key)
+  for (const key of bees) downloadBee(key)
+  for (const key of drives) downloadDrive(key)
 
-  for (const key of bees) {
-    downloadBee(key)
-  }
-
-  for (const key of drives) {
-    downloadDrive(key)
-  }
-
-  for (const s of seeders) {
-    const publicKey = HypercoreId.decode(s)
+  for (const key of seeders) {
+    const publicKey = HypercoreId.decode(key)
     const id = HypercoreId.encode(publicKey)
     const keyPair = await store.createKeyPair('simple-seeder-swarm@' + id)
-    const sw = new Seeders(publicKey, {
-      dht: swarm.dht,
-      keyPair: keyPair
-    })
+    const sw = new Seeders(publicKey, { dht: swarm.dht, keyPair: keyPair })
 
-    console.log('Seeder swarm for ' + s + ' listening on ' + HypercoreId.encode(keyPair.publicKey))
+    tracking.seeders.push(sw)
+    update()
 
     sw.join()
     sw.on('connection', onsocket)
-    sw.on('update', function (record) {
-      console.log('seeder change for ' + s + ':', record)
-    })
+    /* sw.on('update', function (record) {
+      console.log('seeder change for ' + key + ':', record)
+    }) */
 
     goodbye(() => sw.destroy())
 
-    if (!drives.includes(s)) {
-      downloadDrive(s, false)
+    if (!drives.includes(key)) {
+      downloadDrive(key, false)
     }
   }
 
   async function downloadDrive (key, announce) {
     const driveId = HypercoreId.encode(HypercoreId.decode(key))
     const drive = new Hyperdrive(store, HypercoreId.decode(key))
-    drive.on('blobs', blobs => downloadCore(blobs.core, driveId, false))
-    console.log('downloading drive', driveId)
-    downloadBee(drive.core, announce)
+    drive.on('blobs', blobs => downloadCore(blobs.core, false, { track: false }))
+    downloadBee(drive.core, announce, { track: false })
+
+    const info = { drive, download: speedometer(), upload: speedometer() }
+    drive.core.on('download', () => info.download(1))
+    drive.core.on('upload', () => info.upload(1))
+    drive.on('blobs', blobs => {
+      blobs.core.on('download', () => info.download(1))
+      blobs.core.on('upload', () => info.upload(1))
+    })
+    tracking.drives.push(info)
+    update()
   }
 
-  async function downloadBee (core, announce) {
+  async function downloadBee (core, announce, { track = true } = {}) {
     core = typeof core === 'string' ? store.get(HypercoreId.decode(core)) : core
 
     const bee = new Hyperbee(core)
     await bee.ready()
 
-    downloadCore(core, null, announce, 'bee')
+    downloadCore(core, announce, { track, isBee: true })
+
+    if (track) {
+      const info = { bee, download: speedometer(), upload: speedometer() }
+      core.on('download', () => info.download(1))
+      core.on('upload', () => info.upload(1))
+      tracking.bees.push(info)
+      update()
+    }
   }
 
-  async function downloadCore (core, driveId, announce, name) {
+  async function downloadCore (core, announce, { track = true, isBee = false } = {}) {
     core = typeof core === 'string' ? store.get(HypercoreId.decode(core)) : core
-    await core.ready()
-    const id = HypercoreId.encode(core.key)
-    console.log('downloading', name || 'core', id)
 
-    if (announce !== false) {
-      console.log('announcing', id)
-      swarm.join(core.discoveryKey)
+    if (track && !isBee) {
+      const info = { core, download: speedometer(), upload: speedometer() }
+      core.on('download', () => info.download(1))
+      core.on('upload', () => info.upload(1))
+      tracking.cores.push(info)
+      update()
     }
-    core.download()
 
-    core.on('download', function (index) {
-      const driveDataTag = driveId ? 'with drive ' + driveId : ''
-      console.log('downloaded block', index, 'from', id, driveDataTag)
-    })
+    await core.ready()
+    if (announce !== false) swarm.join(core.discoveryKey)
+    core.download({ linear: true })
   }
 
   goodbye(() => swarm.destroy())
 
   function onsocket (socket) {
-    const p = HypercoreId.encode(socket.remotePublicKey)
+    const remoteInfo = socket.rawStream.remoteHost + ':' + socket.rawStream.remotePort
+    const id = HypercoreId.encode(socket.remotePublicKey)
 
-    connections++
-
-    console.log('Connection opened', p, '(total ' + connections + ')')
+    // + error logs get lost due clear
+    socket.on('error', (err) => console.log(err))
     store.replicate(socket)
-    socket.on('error', function (err) {
-      console.log(err)
-    })
-    socket.on('close', function () {
-      connections--
-      console.log('Connection closed', p, '(total ' + connections + ')')
-    })
   }
 
-  await swarm.dht.ready()
+  watch()
+}
 
-  console.log()
-  console.log('Node info:')
-  console.log('- remote host:', swarm.dht.host)
-  console.log('- remote port:', swarm.dht.port)
-  console.log('- firewalled:', swarm.dht.firewalled)
-  console.log('- nat type:', swarm.dht.port ? 'consistent' : 'random')
-  console.log()
+function watch () {
+  const { swarm, seeders, cores, bees, drives } = tracking
+  const { dht } = swarm
+
+  // tracking.intervalId = setInterval(update, 50)
+  update()
+
+  const DHT_EVENTS = ['listening', 'ready', 'close', 'nat-update', 'ephemeral', 'persistent', 'wakeup', 'network-change']
+  const SWARM_EVENTS = ['connection', 'update']
+  const CORE_EVENTS = ['ready', 'close', 'conflict', 'truncate', 'append', 'download', 'upload', 'peer-add', 'peer-remove']
+
+  for (const event of DHT_EVENTS) dht.on(event, update)
+  for (const event of SWARM_EVENTS) swarm.on(event, update)
+  // + seeders
+
+  for (const { core } of cores) watchCore(core)
+  for (const { bee } of bees) watchCore(bee.core)
+  for (const { drive } of drives) {
+    watchCore(drive.core)
+
+    if (drive.blobs) watchCore(drive.blobs.core)
+    else drive.on('blobs', blobs => watchCore(blobs.core))
+  }
+
+  function watchCore (core) {
+    for (const event of CORE_EVENTS) core.on(event, update)
+  }
+}
+
+function update () {
+  const { swarm, seeders, cores, bees, drives } = tracking
+  const { dht } = swarm
+
+  if (!dht || !dht.bootstrapped) return
+
+  let output = ''
+  const print = (...args) => output += args.join(' ') + '\n'
+
+  console.clear()
+
+  print('Node')
+  print('- Address:', crayon.yellow(dht.host + ':' + dht.port))
+  print('- Firewalled?', dht.firewalled ? crayon.red('Yes') : crayon.green('No'))
+  print('- NAT type:', dht.port ? crayon.green('Consistent') : crayon.red('Random'))
+  print()
+
+  print('Swarm')
+  print('- Public key:', crayon.magenta(HypercoreId.encode(swarm.keyPair.publicKey)))
+  print('- Connections:', swarm.connections.size, swarm.connecting ? ('(connecting ' + crayon.yellow(swarm.connecting) + ')') : '')
+  // + more info
+  print()
+
+  print('Seeders')
+  for (const seeder of seeders) {
+    print(HypercoreId.encode(seeder.keyPair.publicKey))
+    // + more info
+  }
+  print()
+
+  print('Cores')
+  for (const { core, download, upload } of cores) {
+    if (!core.opened) continue
+
+    print(
+      '-',
+      crayon.magenta(core.id),
+      crayon.yellow(core.contiguousLength + '/' + core.length),
+      crayon.blueBright('peers ' + core.peers.length),
+      crayon.green('dl ' + Math.ceil(download()) + ' b/s'),
+      crayon.cyan('up ' + Math.ceil(upload()) + ' b/s')
+    )
+  }
+  print()
+
+  print('Bees')
+  for (const { bee, download, upload } of bees) {
+    const { core } = bee
+    if (!core.opened) continue
+
+    print(
+      '-',
+      crayon.magenta(core.id),
+      crayon.yellow(core.contiguousLength + '/' + core.length),
+      crayon.blueBright('peers ' + core.peers.length),
+      crayon.green('dl ' + Math.ceil(download()) + ' b/s'),
+      crayon.cyan('up ' + Math.ceil(upload()) + ' b/s')
+    )
+  }
+  print()
+
+  print('Drives')
+  for (const { drive, download, upload } of drives) {
+    if (!drive.opened || !drive.blobs) continue
+
+    const id = HypercoreId.encode(drive.key)
+    const filesProgress = drive.core.contiguousLength + '/' + drive.core.length
+    const blobsProgress = drive.blobs.core.contiguousLength + '/' + drive.blobs.core.length
+    const peers = drive.core.peers.length + drive.blobs.core.peers.length
+
+    print(
+      '-',
+      crayon.magenta(id),
+      crayon.yellow(filesProgress) + ', ' + crayon.yellow(blobsProgress),
+      crayon.blueBright('peers ' + peers),
+      crayon.green('dl ' + Math.ceil(download()) + ' b/s'),
+      crayon.cyan('up ' + Math.ceil(upload()) + ' b/s')
+    )
+  }
+  print()
+
+  console.log(output)
 }
