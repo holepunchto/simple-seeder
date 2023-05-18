4f2f810b3554c37a2207c8bd139d321122dc4be9
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-05-18 15:13:14 +0200

    Log diskspace usage per core (#14)
    * Log diskspace usage per core
    
    * Use byteSize

--- index.js
+++ index.js
@@ -209,6 +209,7 @@ function update () {
         '-',
         crayon.green(core.id),
         crayon.yellow(core.contiguousLength + '/' + core.length) + ' blks,',
+        crayon.yellow(byteSize(core.byteLength) + ','),
         crayon.yellow(core.peers.length) + ' peers,',
         crayon.green('↓') + ' ' + crayon.yellow(Math.ceil(blocks.download())),
         crayon.cyan('↑') + ' ' + crayon.yellow(Math.ceil(blocks.upload())) + ' blks/s',
@@ -228,6 +229,7 @@ function update () {
         '-',
         crayon.green(core.id),
         crayon.yellow(core.contiguousLength + '/' + core.length) + ' blks,',
+        crayon.yellow(byteSize(core.byteLength) + ','),
         crayon.yellow(core.peers.length) + ' peers,',
         crayon.green('↓') + ' ' + crayon.yellow(Math.ceil(blocks.download())),
         crayon.cyan('↑') + ' ' + crayon.yellow(Math.ceil(blocks.upload())) + ' blks/s',
@@ -244,11 +246,12 @@ function update () {
       const id = HypercoreId.encode(drive.key)
       const filesProgress = drive.core.contiguousLength + '/' + drive.core.length
       const blobsProgress = (drive.blobs?.core.contiguousLength || 0) + '/' + (drive.blobs?.core.length || 0)
-
+      const blobsBytes = drive.blobs?.core.byteLength || 0
       print(
         '-',
         crayon.green(id),
         crayon.yellow(filesProgress) + ' + ' + crayon.yellow(blobsProgress) + ' blks,',
+        crayon.yellow(byteSize(blobsBytes) + ','),
         crayon.yellow(drive.core.peers.length) + ' + ' + crayon.yellow(drive.blobs?.core.peers.length || 0) + ' peers,',
         crayon.green('↓') + ' ' + crayon.yellow(Math.ceil(blocks.download())),
         crayon.cyan('↑') + ' ' + crayon.yellow(Math.ceil(blocks.upload())) + ' blks/s',
